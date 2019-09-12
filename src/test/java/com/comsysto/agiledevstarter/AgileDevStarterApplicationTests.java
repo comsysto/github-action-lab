c4eefc4eaafe43f50cca9343a9ca0823679248ee
sfleischer <s.fleischer@reply.de>
2019-09-12 15:48:08 +0200

    Renamed application, added Cloud Foundry manifest file and created quick guide for Cloud Foundry commands

--- src/test/java/com/comsysto/agiledevstarter/AgileDevStarterApplicationTests.java
+++ src/test/java/com/comsysto/agiledevstarter/AgileDevStarterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.comsysto.agilestarterbundle;
+package com.comsysto.agiledevstarter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AgilestarterbundleApplicationTests {
+public class AgileDevStarterApplicationTests {
 
     @Test
     public void contextLoads() {
